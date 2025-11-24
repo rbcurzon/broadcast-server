f7c986c565a389a421323a0e7b0134b48cea7878
rbcurzon_laptop <curzonronald@gmail.com>
2025-11-24 18:36:39 +0800

    initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+    	
+    }
+}
---
+++ src/test/java/com/mycompany/app/UDPTest.java
@@ -0,0 +1,22 @@
+package com.mycompany.app;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UDPTest {
+
+	@BeforeEach
+	public void setup() throws IOException {
+	}
+
+	@Test
+	public void whenCanPublishAndReceivePacket_thenCorrect() throws IOException {
+	}
+
+	@AfterEach
+	public void tearDown() throws IOException {
+	}
+}
